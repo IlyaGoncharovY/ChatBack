b829164da90745fd341c3b7bc542b211d4d44ee4
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-01-08 14:43:25 +0300

    fix(error ed) config error

--- src/app.ts
+++ src/app.ts
@@ -7,14 +7,23 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
 
 app.get('/', (req, res) => {
-    res.send('Its ws server');
+    res.send('Its ws server!!!2222');
 })
 
 io.on("connection", (socket) => {
-    // ...
-    console.log('a user connected')
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('error', (error) => {
+        console.error('Socket error:', error);
+    });
 });
 
-httpServer.listen(3009, () => {
+const PORT = process.env.PORT || 3009;
+
+httpServer.listen(PORT, () => {
     console.log('Listening on 3009 port')
 });
