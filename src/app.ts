4fe501fa68bf8e240122b6437a7396369fe6c37b
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-01-08 17:11:41 +0300

    fix(get message) get message from server

--- src/app.ts
+++ src/app.ts
@@ -17,8 +17,20 @@ app.get('/', (req, res) => {
     res.send('Its ws server!!!444');
 });
 
+const messages = [
+    {message: 'hello Vasya', id: 'e1q1q1q', user: {id: '22w2w2ww', name: 'Ilya'}},
+    {message: 'hello Ilya', id: '1a1a1a', user: {id: '2s2s2s', name: 'Vasya'}},
+    {message: 'hello Friends', id: '3a3a3a', user: {id: '3s3s3s', name: 'Vova'}},
+]
+
 io.on("connection", (socket) => {
     console.log('a user connected');
+
+    socket.on('click-message-emit', (message: string) => {
+        console.log(message)
+    })
+
+    socket.emit('init-messages-published', messages)
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
