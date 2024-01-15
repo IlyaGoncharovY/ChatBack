bc00639961864a3f0ca75c8305b7e9f57238fb7c
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-01-15 23:55:11 +0300

    fix(for "render") origin

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,8 @@ const app = express();
 const httpServer = createServer(app);
 const socketIO = new Server(httpServer, {
     cors: {
-        origin: 'http://localhost:5173',
+        // origin: 'http://localhost:5173',
+        origin: 'https://ilyagoncharovy.github.io/ChatFront/',
     },
 });
 
